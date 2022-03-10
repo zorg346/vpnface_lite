ef1b77c27c94e723333fd76eb4b14389d0d147be
zorg346 <99472832+zorg346@users.noreply.github.com>
2022-03-10 21:13:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,24 +3,24 @@
 #installation path
 #this is not data path. data path configured with conf.js
 VFL_DIR=/opt/vpnface_lite
-VFL_GIT=https://github.com/abrakadobr/vpnface_lite
-
+#VFL_GIT=https://github.com/abrakadobr/vpnface_lite
+VFL_GIT=https://github.com/zorg346/vpnface_lite
 #install updates and packages
 apt-get update
 apt-get upgrade -y
 apt-get install -y lsb-release git tor nginx iptables-persistent openvpn
 # openvpn: install stable version from official repo
 #wget -O - https://swupdate.openvpn.net/repos/repo-public.gpg|apt-key add -
-#echo "deb http://build.openvpn.net/debian/openvpn/release/2.4 xenial main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
+#echo "deb http://build.openvpn.net/debian/openvpn/release/2.5 xenial main" > /etc/apt/sources.list.d/openvpn-aptrepo.list
 #apt-get update
 #apt-get install -y openvpn easy-rsa
 
-#fast fix for openvpn2.4 and easyrsa3
+#fast fix for openvpn2.5 and easyrsa3
 #get latest easy-rsa manually
-wget -P /opt/ https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.4/EasyRSA-3.0.4.tgz
-cd /opt/ && tar xvf EasyRSA-3.0.4.tgz
+wget -P /opt/ https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz
+cd /opt/ && tar xvf EasyRSA-3.0.8.tgz
 mkdir /usr/share/easy-rsa
-cp -r /opt/EasyRSA-3.0.4/* /usr/share/easy-rsa/
+cp -r /opt/EasyRSA-3.0.8/* /usr/share/easy-rsa/
 
 #and create logs folder
 mkdir -p /var/log/openvpn
@@ -29,7 +29,7 @@ mkdir -p /var/log/openvpn
 #
 #
 #installation node version manager and node 10
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.39.0/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
